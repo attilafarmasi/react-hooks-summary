d35e4d27909c303e6d03f848dcc07297f3833fb3
Farmasi Attila <attila.farmasi@dpdgroup.com>
2022-12-07 15:05:23 +0100

    useEffect() and Loading data

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
@@ -7,21 +7,41 @@ import Search from "./Search";
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
+  useEffect(() => {
+    fetch(
+      "https://react-hooks-update-12232-default-rtdb.firebaseio.com/ingredients.json")
+      .then((response) => response.json())
+      .then((responseData) => {
+        const loadedIngredients = [];
+        for (const key in responseData) {
+          loadedIngredients.push({
+            id: key,
+            title: responseData[key].title,
+            amount: responseData[key].amount,
+          });
+        }
+        setUserIngredients(loadedIngredients);
+      });
+  }, []);
+
   const addIgredientHandler = (ingredient) => {
     fetch(
-      "https://react-hooks-update-12232-default-rtdb.firebaseio.com/ingredients.json", {
+      "https://react-hooks-update-12232-default-rtdb.firebaseio.com/ingredients.json",
+      {
         method: "POST",
         body: JSON.stringify(ingredient),
         headers: { "Content-Type": "application/json" },
       }
-    ).then((response) => {
-      return response.json();
-    }).then(responseData => {
-      setUserIngredients((prevIngredients) => [
-        ...prevIngredients,
-        { id: responseData.name, ...ingredient },
-      ]);
-    });
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((responseData) => {
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: responseData.name, ...ingredient },
+        ]);
+      });
   };
 
   const removeIngredientHandler = (id) => {
